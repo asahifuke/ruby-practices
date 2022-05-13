807e9b9bc0d555fa4eb19d1c9fc3ddedf38db694
asahifuke <asahi.fuke@medpeer.co.jp>
2022-05-13 14:17:29 +0900

    rjustなどを用いて、空白を表現しました

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,41 +5,32 @@ require 'optparse'
 require 'Date'
 
 class Calendar
-  def convert_from_cwday_to_number_of_blank_spaces(cwday)
-    {
-      1 => 3,
-      2 => 6,
-      3 => 9,
-      4 => 12,
-      5 => 15,
-      6 => 18,
-      7 => 0
-    }[cwday]
-  end
+  DAY_OF_WEEK = '日 月 火 水 木 金 土'
+  SATURDAY    = 6
 
   def initialize
-    @options               = ARGV.getopts('y:', 'm:')
-    @today                 = Date.today
-    @month                 = @options['m'] || @today.month
-    @year                  = @options['y'] || @today.year
+    @options = ARGV.getopts('y:', 'm:')
   end
 
   def show
-    @start_date            = Date.new(@year.to_i, @month.to_i,  1)
-    @finish_date           = Date.new(@year.to_i, @month.to_i, -1)
-    @blank_space           = ' '
-    @number_of_blank_space = 6
-    @saturday              = 6
-    puts @blank_space * @number_of_blank_space + "#{@month}月 #{@year}"
-    puts '日 月 火 水 木 金 土'
-    (@start_date..@finish_date).each do |current_date|
-      print @blank_space if [1, 2, 3, 4, 5, 6, 7, 8, 9].include?(current_date.day)
-      print @blank_space * convert_from_cwday_to_number_of_blank_spaces(current_date.cwday) if current_date == @start_date
-      if current_date.cwday == @saturday || current_date == @finish_date
-        puts current_date.day
+    today       = Date.today
+    month       = @options['m'] || today.month
+    year        = @options['y'] || today.year
+    start_date  = Date.new(year.to_i, month.to_i,  1)
+    finish_date = Date.new(year.to_i, month.to_i, -1)
+
+    puts "#{month}月 #{year}".center(20)
+    puts DAY_OF_WEEK
+
+    (start_date..finish_date).each do |current_date|
+      current_day  = current_date.day.to_s.rjust(2).ljust(3)
+      current_wday = current_date.wday
+      if current_date == start_date
+        print current_day.rjust((current_wday + 1) * 3)
+      elsif current_wday == SATURDAY || current_date == finish_date
+        puts current_day
       else
-        print current_date.day
-        print @blank_space
+        print current_day
       end
     end
   end
@@ -47,4 +38,3 @@ end
 
 calendar = Calendar.new
 calendar.show
-
