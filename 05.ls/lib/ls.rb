fcac78ef8d9d86672b3873a66c34e1d837345551
asahifuke <asahi.fuke@medpeer.co.jp>
2022-06-20 13:43:11 +0900

    detachの内容をinitializeに統合しました

--- 05.ls/lib/ls.rb
+++ 05.ls/lib/ls.rb
@@ -6,27 +6,17 @@ class List
   NUMBER_OF_COLUMNS = 3
 
   def initialize
-    @files_directories = Dir.glob('*')
-    size = @files_directories.size
+    files_directories = Dir.glob('*')
+    size = files_directories.size
     @number_of_files_in_a_column = size / NUMBER_OF_COLUMNS
     @number_of_files_in_a_column += 1 unless (size % NUMBER_OF_COLUMNS).zero?
     @columns = Array.new(NUMBER_OF_COLUMNS) { Column.new }
-  end
-
-  def show
-    detach
-    attach
-  end
-
-  private
-
-  def detach
-    @files_directories.each_with_index do |file, i|
+    files_directories.each_with_index do |file, i|
       @columns[i / @number_of_files_in_a_column].add(file)
     end
   end
 
-  def attach
+  def show
     Array.new(@number_of_files_in_a_column) do |i|
       @columns.map { |column| column.show(i) }.join('   ')
     end.join("\n")
