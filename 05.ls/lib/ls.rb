2786e9bf9af2ddb8450b1776c5e534d96e51e700
asahifuke <asahi.fuke@medpeer.co.jp>
2022-06-21 16:24:56 +0900

    -aの追加

--- 05.ls/lib/ls.rb
+++ 05.ls/lib/ls.rb
@@ -1,26 +1,9 @@
 # frozen_string_literal: true
 
-require_relative './column'
+require 'optparse'
+require_relative './list'
 
-class List
-  NUMBER_OF_COLUMNS = 3
-
-  def initialize
-    files_directories = Dir.glob('*')
-    size = files_directories.size
-    @number_of_files_in_a_column = size / NUMBER_OF_COLUMNS
-    @number_of_files_in_a_column += 1 unless (size % NUMBER_OF_COLUMNS).zero?
-    @columns = Array.new(NUMBER_OF_COLUMNS) { Column.new }
-    files_directories.each_with_index do |file, i|
-      @columns[i / @number_of_files_in_a_column].add(file)
-    end
-  end
-
-  def show
-    Array.new(@number_of_files_in_a_column) do |i|
-      @columns.map { |column| column.show(i) }.join('   ')
-    end.join("\n")
-  end
-end
-
-puts List.new.show
+options = ARGV.getopts('a')
+args = ['*']
+args << File::FNM_DOTMATCH if options['a']
+puts List.new(args).show
--- 05.ls/test/ls_test.rb
+++ 05.ls/test/ls_test.rb
@@ -4,12 +4,15 @@ require 'minitest/autorun'
 require_relative '../lib/ls'
 
 class ListTest < Minitest::Test
-  def setup
-    @list = List.new
+  def test_normal
+    list = List.new(['*'])
+    output = 'column_test.rb   ls_test.rb   '
+    assert_equal output, list.show
   end
 
-  def test_show
-    output1 = 'column_test.rb   ls_test.rb   '
-    assert_equal output1, @list.show
+  def test_a
+    list = List.new(['*', File::FNM_DOTMATCH])
+    output = ".           column_test.rb   \n.DS_Store   ls_test.rb       "
+    assert_equal output, list.show
   end
 end
