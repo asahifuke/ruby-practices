778c8665ec83a083c268c2a7f87f69418ba7e7f5
asahifuke <asahi.fuke@medpeer.co.jp>
2022-07-13 15:59:17 +0900

    -lを追加しました

--- 05.ls/lib/ls.rb
+++ 05.ls/lib/ls.rb
@@ -6,4 +6,4 @@ require_relative './list'
 options = ARGV.getopts('a', 'r', 'l')
 args = ['*']
 args << File::FNM_DOTMATCH if options['a']
-puts List.new(args, options['r']).show
+puts List.new(args, options['r'], options['l']).show
--- 05.ls/test/ls_test.rb
+++ 05.ls/test/ls_test.rb
@@ -5,37 +5,52 @@ require_relative '../lib/ls'
 
 class ListTest < Minitest::Test
   def test_normal
-    list = List.new(['*'], false)
+    list = List.new(['*'], false, false)
     output = 'column_test.rb   ls_test.rb   '
     assert_equal output, list.show
   end
 
   def test_a
-    list = List.new(['*', File::FNM_DOTMATCH], false)
-    output = ".           column_test.rb   \n.DS_Store   ls_test.rb       "
+    list = List.new(['*', File::FNM_DOTMATCH], false, false)
+    output = '.           column_test.rb   \n.DS_Store   ls_test.rb       '
     assert_equal output, list.show
   end
 
   def test_r
-    list = List.new(['*'], true)
+    list = List.new(['*'], true, false)
     output = 'ls_test.rb   column_test.rb   '
     assert_equal output, list.show
   end
 
   def test_ar
-    list = List.new(['*', File::FNM_DOTMATCH], true)
-    output = "ls_test.rb       .DS_Store   \ncolumn_test.rb   .           "
+    list = List.new(['*', File::FNM_DOTMATCH], true, false)
+    output = 'ls_test.rb       .DS_Store   \ncolumn_test.rb   .           '
     assert_equal output, list.show
   end
 
   def test_l
-    list = List.new(['*', File::FNM_DOTMATCH], true)
-    output = <<~TEXT
-    total 24
-    -rw-r--r--  1 asahi.fuke  staff  394  7 12 23:20 ls.rb
-    -rw-r--r--  1 asahi.fuke  staff  722  7 12 23:16 list.rb
-    -rw-r--r--  1 asahi.fuke  staff  349  7 12 23:16 column.rb
-    TEXT
+    list = List.new(['*'], false, true)
+    output = ['total 16', '-rw-r--r--  1 asahi.fuke  staff   540  7 13 10:56 column_test.rb', '-rw-r--r--  1 asahi.fuke  staff  1440  7 13 15:00 ls_test.rb']
+    assert_equal output, list.show
+  end
+
+  def test_lr
+    list = List.new(['*'], true, true)
+    output = ['total 16', '-rw-r--r--  1 asahi.fuke  staff   540  7 13 10:56 column_test.rb', '-rw-r--r--  1 asahi.fuke  staff  1694  7 13 15:02 ls_test.rb']
+    assert_equal output, list.show
+  end
+
+  def test_al
+    list = List.new(['*', File::FNM_DOTMATCH], false, true)
+    output = ['total 32', 'drwxr-xr-x  5 asahi.fuke  staff   160  7 13 10:56 .', '-rw-r--r--  1 asahi.fuke  staff  6148  6 09 12:54 .DS_Store',
+              '-rw-r--r--  1 asahi.fuke  staff   540  7 13 10:56 column_test.rb', '-rw-r--r--  1 asahi.fuke  staff  1440  7 13 15:00 ls_test.rb']
+    assert_equal output, list.show
+  end
+
+  def test_alr
+    list = List.new(['*', File::FNM_DOTMATCH], true, true)
+    output = ['total 32', 'drwxr-xr-x  5 asahi.fuke  staff   160  7 13 10:56 .', '-rw-r--r--  1 asahi.fuke  staff  6148  6 09 12:54 .DS_Store',
+              '-rw-r--r--  1 asahi.fuke  staff   540  7 13 10:56 column_test.rb', '-rw-r--r--  1 asahi.fuke  staff  2085  7 13 15:03 ls_test.rb']
     assert_equal output, list.show
   end
 end
