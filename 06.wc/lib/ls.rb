6b050f9b932eabda510351ca5501e57152163677
asahifuke <asahi.fuke@medpeer.co.jp>
2022-07-25 12:59:58 +0900

    lsを削除しました

--- 06.wc/lib/ls.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'optparse'
-require_relative '../lib/ls/list'
-
-options = ARGV.getopts('a', 'r', 'l')
-args = ['*']
-args << File::FNM_DOTMATCH if options['a']
-puts List.new(args, options['r'], options['l']).show
